6a81cfd15bffec4260aeb643abb8f9cf8ea898d7
Alex Clarke <amc.kjg@gmail.com>
2021-12-22 00:34:06 +0000

    Day 15

---
+++ day15/part2.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+my $tbl = getInput();
+printGrid($tbl, {});
+
+my $table = multiplyInput($tbl, 5);
+
+printGrid($table, {});
+print "After print grid\n";
+
+my %nodes;
+my ($y, $x) = (0, 0);
+for my $row (@$table) {
+	for my $cell (@$row) {
+		$nodes{"$y,$x"} = {};
+		$x++;
+	}
+	$x = 0;
+	$y++;
+}
+
+($y, $x) = (0, 0);
+for my $row (@$table) {
+	for my $cell (@$row) {
+		if ($y > 0) {
+			my $lowY = $y - 1;
+			$nodes{"$lowY,$x"}->{"$y,$x"} = $cell; 
+		}
+		if ($x > 0) {
+			my $lowX = $x - 1;
+			$nodes{"$y,$lowX"}->{"$y,$x"} = $cell; 
+		}
+		if ($y < (scalar(@$table)-1)) {
+			my $highY = $y + 1;
+			$nodes{"$highY,$x"}->{"$y,$x"} = $cell; 
+		}
+		if ($x < (scalar(@$row)-1)) {
+			my $highX = $x + 1;
+			$nodes{"$y,$highX"}->{"$y,$x"} = $cell; 
+		}
+		$x++;
+	}
+	$x = 0;
+	$y++;
+}
+print Dumper(\%nodes);
+
+my ($costs, $prev) = dijkstra(\%nodes, "0,0");
+print Dumper($costs);
+
+my %prevShort;
+
+my $node = "". (scalar(@$table)-1) .",". (scalar(@{@$table[0]})-1);
+print "Cost for getting to dest: ". $costs->{$node} ."\n";
+do {
+	$prevShort{$node} = 1;
+	$node = $prev->{$node};
+	print "node $node\n";
+} until ($node eq '0,0');
+
+
+printGrid($table, \%prevShort);
+
+sub dijkstra {
+	my ($graph, $source) = @_;
+
+	my %dist = ($source => 0);
+	my %prev;
+
+	my @q = ($source);  # All nodes initially in Q
+	while (@q) {
+		my $v = shift @q;
+		my @a2q;
+		for (keys %{$graph->{$v}}) {
+			if ($dist{$_} != undef) {
+				my $alt = $dist{$v} + $graph->{$v}->{$_};
+				if ($alt < $dist{$_}) {
+					$dist{$_} = $alt;
+					$prev{$_} = $v;
+				} 
+			} else {
+				push @q, $_;
+				$dist{$_} = $dist{$v} + $graph->{$v}->{$_};
+				$prev{$_} = $v;
+			}
+			@q = sort { ($dist{$a}) <=> ($dist{$b}) } @q;
+		}
+	}
+	return \%dist, \%prev;
+}
+
+sub printGrid {
+	my ($grid, $blanks) = @_;
+
+	my ($y, $x) = (0, 0);
+	for (@$grid) {
+		for (@$_) {
+			if ($blanks->{"$y,$x"}) {
+				print '.';
+			} else {
+				print;
+			}
+			$x++;
+		}
+		$x = 0;
+		$y++;
+		print "\n";
+	}
+}
+
+sub getInput() {
+	my $tbl = [];
+	while (<>) {
+		chomp;
+		my @line;
+		push @line, $_ for (split '', $_);
+		push @$tbl, \@line;
+	}
+	return $tbl;
+}
+
+sub multiplyInput() {
+	my ($tbl, $by) = @_;
+
+	my @lgTbl;
+
+	my $i = 0;
+	for (@$tbl) {
+		my @copy = @$_;
+		@lgTbl[$i++] = \@copy;
+	}
+	my $tblRowCount = scalar @$tbl;
+
+	for my $y (0..$by-1) {
+		if ($y > 0) {
+			push @lgTbl, [] for (1..$tblRowCount);
+		}
+		for my $x (0..$by-1) {
+			unless ($y == 0 && $x == 0) {
+				my $appToY = 0;
+				for (@$tbl) {
+					for (@$_) {
+						my $riskLvl = ($_+($x+$y));
+						$riskLvl = $riskLvl - 9 if ($riskLvl > 9);
+						push @{@lgTbl[$appToY+($y*$tblRowCount)]}, $riskLvl;
+					}
+					$appToY++;
+				}
+			}
+		}
+	}
+	print "Return a table with ". scalar(@lgTbl) ." rows, that are ". scalar(@{$lgTbl[0]}) ." wide\n";
+	return \@lgTbl;
+}
